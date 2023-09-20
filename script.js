7fb086a497ab73096c030dde4064c16a55e920b5
Maxnikit <max.nikit.03@gmail.com>
2023-09-21 02:34:03 +0300

    Add first stage of 10% extra task

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let chosenSize = 16;
 let chosenColor = "#393E46";
 let lastChosenColor = "#393E46";
 let rainbowMode = false;
+let newRgbArray = [];
 const container = document.getElementById("container");
 const boxColor = document.getElementById("colorPicker");
 const rainbowButton = document.getElementById("rainbowButton");
@@ -72,4 +73,38 @@ function randomRgbColor() {
   let g = Math.floor(Math.random() * 256); // Random between 0-255
   let b = Math.floor(Math.random() * 256); // Random between 0-255
   return "rgb(" + r + "," + g + "," + b + ")";
+}
+console.log(chosenColor);
+
+const hexToRgb = (hex) =>
+  hex
+    .replace(
+      /^#?([a-f\d])([a-f\d])([a-f\d])$/i,
+      (m, r, g, b) => "#" + r + r + g + g + b + b
+    )
+    .substring(1)
+    .match(/.{2}/g)
+    .map((x) => parseInt(x, 16));
+
+// TODO Разобраться с затемнением(по 10% за наводку). Пока что получается хреново
+for (let iOut = 0; iOut < 3; iOut++) {
+  let originalN = hexToRgb(chosenColor)[iOut];
+  let modifiedN = originalN;
+  console.log(originalN);
+  tenPercent = Math.floor((originalN / 100) * 10);
+  console.log(tenPercent);
+  for (let i = 0; i < 20; i++) {
+    if (modifiedN > 0) {
+      modifiedN -= tenPercent;
+      newRgb = modifiedN;
+      newRgbArray.splice(iOut, 1, newRgb);
+    } else {
+      modifiedN = 0;
+      newRgb = modifiedN;
+      newRgbArray.splice(iOut, 1, newRgb);
+    }
+    console.log(newRgb);
+  }
+  console.log(modifiedN);
+  console.log(newRgbArray);
 }
